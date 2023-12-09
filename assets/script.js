1ff2b6808defab7eaf4c2f05065ea12453f56088
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-09 14:53:46 -0300

    Aula IA - 2

--- assets/script.js
+++ assets/script.js
@@ -25,13 +25,62 @@ setTimeout(() => {
 }, 1000)
 
 setInterval(() => {
-  if (!game.activated) return
+  if (!game.activated) return;
 
-  const dino = dinoList[dinoIndex]
+  const dino = dinoList[dinoIndex];
 
   if (game.crashed) {
     if(dino.score > bestScore) {
-      bestScore = dino.score
+      bestScore = dino.score;
+      bestRNA = dino.save();
+      console.log('Melhor Pontuação: ', bestScore);
     }
+    dinoIndex++;
+
+  if (dinoIndex === SAMPLES) {
+    fillDinoList();
+    dinoIndex = 0;
+    bestScore = 0;
   }
-})
+  game.restart();
+}
+
+ const {tRex, horizon, currentSpeed, distanceRan, dimensions} = game;
+ dino.score = distanceRan - 2000;
+
+ const player = {
+  x: tRex.xPos,
+  y: tRex.yPos,
+  speed: currentSpeed
+ };
+
+ const [obstacle] = horizon.obstacles
+    .map((obstacle) => {
+    return {
+      x: obstacle.xPos,
+      y: obstacle.yPos
+    }
+  })
+    .filter((obstacle) => obstacle.x > player.x)
+
+    if (obstacle) {
+      const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
+      const speed = player.speed / 6;
+      const height = Math.tanh(105 - obstacle.y)
+
+      const [jump, crounch] = dino.compute([
+        distance,
+        speed,
+        height
+      ]);
+
+      if (jump === crounch) return;
+      if (jump) controls.dispatch('jump'); // Se for verdadeiro o dinossauro pula
+      if (crounch) controls.dispatch('crounch');
+    };
+}, 100);
+
+/* const s = document.createElement('scrept');
+s.type = 'module';
+s.src = 'http://localhost:5500/script.js'
+document.body.appendChild(s); */
